e139d53776a60e3395691a8785041f896e497562
zhangzhaoju <zzjj2008vi@126.com>
2025-12-31 17:21:06 +0800

    optimize start_regress_test.sh, stop test if any command failed

--- tests/regress/start_regress_test.sh
+++ tests/regress/start_regress_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
@@ -17,7 +18,8 @@ FALCON_STORE_NUM=1
 function uninstall_cluster() {
     # unmount fuse filesystem before reinstall
     for ((idx = 1; idx <= FALCON_STORE_NUM; idx++)); do
-        sudo umount -l ${FALCON_DATA_PATH}/falcon-data/store-"${idx}"/data
+        # do umount
+        sudo umount -l "${FALCON_DATA_PATH}"/falcon-data/store-"${idx}"/data 2>/dev/null || true
     done
     # Stop and remove pre containers resources
     docker-compose -f "${1}" down
@@ -26,67 +28,72 @@ function uninstall_cluster() {
 function clean_run_data() {
     # clear history data of zk node
     for ((idx = 1; idx <= FALCON_ZK_NUM; idx++)); do
-        if [ -d ${FALCON_DATA_PATH}/falcon-data/zk-"${idx}"/data ]; then
-            mkdir -p ${FALCON_DATA_PATH}/falcon-data/zk-"${idx}"/data/
+        if [ -d "${FALCON_DATA_PATH}"/falcon-data/zk-"${idx}"/data ]; then
+            mkdir -p "${FALCON_DATA_PATH}"/falcon-data/zk-"${idx}"/data/
         else
-            sudo rm -rf ${FALCON_DATA_PATH}/falcon-data/zk-"${idx}"/data/*
+            sudo rm -rf "${FALCON_DATA_PATH}"/falcon-data/zk-"${idx}"/data/*
         fi
     done
 
     # clear history data of cn node
     for ((idx = 1; idx <= FALCON_CN_NUM; idx++)); do
-        if [ -d ${FALCON_DATA_PATH}/falcon-data/cn-"${idx}"/data ]; then
-            sudo rm -rf ${FALCON_DATA_PATH}/falcon-data/cn-"${idx}"/data/*
+        if [ -d "${FALCON_DATA_PATH}"/falcon-data/cn-"${idx}"/data ]; then
+            sudo rm -rf "${FALCON_DATA_PATH}"/falcon-data/cn-"${idx}"/data/*
         else
-            mkdir -p ${FALCON_DATA_PATH}/falcon-data/cn-"${idx}"/data/
+            mkdir -p "${FALCON_DATA_PATH}"/falcon-data/cn-"${idx}"/data/
         fi
     done
 
     # clear history data of dn node
     for ((idx = 1; idx <= FALCON_DN_NUM; idx++)); do
-        if [ -d ${FALCON_DATA_PATH}/falcon-data/dn-"${idx}"/data ]; then
-            sudo rm -rf ${FALCON_DATA_PATH}/falcon-data/dn-"${idx}"/data/*
+        if [ -d "${FALCON_DATA_PATH}"/falcon-data/dn-"${idx}"/data ]; then
+            sudo rm -rf "${FALCON_DATA_PATH}"/falcon-data/dn-"${idx}"/data/*
         else
-            mkdir -p ${FALCON_DATA_PATH}/falcon-data/dn-"${idx}"/data/
+            mkdir -p "${FALCON_DATA_PATH}"/falcon-data/dn-"${idx}"/data/
         fi
     done
 
     # clear history data of store node
     for ((idx = 1; idx <= FALCON_STORE_NUM; idx++)); do
-        if [ -d ${FALCON_DATA_PATH}/falcon-data/store-"${idx}" ]; then
-            sudo rm -rf ${FALCON_DATA_PATH}/falcon-data/store-"${idx}"/cache/*
-            sudo rm -rf ${FALCON_DATA_PATH}/falcon-data/store-"${idx}"/log/*
-            sudo rm -rf ${FALCON_DATA_PATH}/falcon-data/store-"${idx}"/data/*
+        if [ -d "${FALCON_DATA_PATH}"/falcon-data/store-"${idx}" ]; then
+            sudo rm -rf "${FALCON_DATA_PATH}"/falcon-data/store-"${idx}"/cache/*
+            sudo rm -rf "${FALCON_DATA_PATH}"/falcon-data/store-"${idx}"/log/*
+            sudo rm -rf "${FALCON_DATA_PATH}"/falcon-data/store-"${idx}"/data/*
         else
-            mkdir -p ${FALCON_DATA_PATH}/falcon-data/store-"${idx}"/cache/*
-            mkdir -p ${FALCON_DATA_PATH}/falcon-data/store-"${idx}"/log/*
-            mkdir -p ${FALCON_DATA_PATH}/falcon-data/store-"${idx}"/data/*
+            mkdir -p "${FALCON_DATA_PATH}"/falcon-data/store-"${idx}"/cache/*
+            mkdir -p "${FALCON_DATA_PATH}"/falcon-data/store-"${idx}"/log/*
+            mkdir -p "${FALCON_DATA_PATH}"/falcon-data/store-"${idx}"/data/*
         fi
     done
 }
 
 function rebuild_falcon() {
     # rebuild falconfs using "cloud_native/docker_build/docker_build.sh" manually.
-    cd ${FALCON_CODE_PATH}/third_party/postgres
+    cd "${FALCON_CODE_PATH}"/third_party/postgres
     git restore .
     sudo rm -rf contrib/falcon/
-    cd ${FALCON_CODE_PATH}
+    cd "${FALCON_CODE_PATH}"
     git pull --rebase
     ./patches/apply.sh
+
+    up_flag=$(docker ps -a --filter "name=falcon-dev" --format "{{.Names}}\t{{.Status}}" | awk "/Up/" | wc -l)
+    if [ "$up_flag" -eq 0 ]; then
+        docker start falcon-dev
+    fi
     docker exec -e LD_LIBRARY_PATH=/usr/local/obs/lib -e CPLUS_INCLUDE_PATH=/usr/local/obs/include falcon-dev \
         /root/code/falconfs/cloud_native/docker_build/docker_build.sh
 }
 
 function rebuild_images() {
     # rebuild cn image
-    cd ${FALCON_CODE_PATH}/cloud_native/docker_build/cn
+    cd "${FALCON_CODE_PATH}"/cloud_native/docker_build/cn
     docker buildx build --platform linux/amd64 \
         -t localhost:5000/falconfs-cn:ubuntu24.04 \
         -f Dockerfile \
         . \
         --push
     # rebuild dn image
-    cd ${FALCON_CODE_PATH}/cloud_native/docker_build/dn
+    cd "${FALCON_CODE_PATH}"/cloud_native/docker_build/dn
     docker buildx build --platform linux/amd64 \
         -t localhost:5000/falconfs-dn:ubuntu24.04 \
         -f Dockerfile \
@@ -94,7 +101,7 @@ function rebuild_images() {
         --push
 
     # rebuild store image
-    cd ${FALCON_CODE_PATH}/cloud_native/docker_build/store
+    cd "${FALCON_CODE_PATH}"/cloud_native/docker_build/store
     docker buildx build --platform linux/amd64 \
         -t localhost:5000/falconfs-store:ubuntu24.04 \
         -f Dockerfile \
@@ -102,7 +109,7 @@ function rebuild_images() {
         --push
 
     # rebuild regress image
-    cd ${FALCON_CODE_PATH}/tests/regress/
+    cd "${FALCON_CODE_PATH}"/tests/regress/
     docker buildx build --platform linux/amd64 \
         -t localhost:5000/falconfs-regress:ubuntu24.04 \
         -f Dockerfile \
@@ -110,12 +117,19 @@ function rebuild_images() {
         --push
 }
 
+function clear_one_image() {
+    rep_info=$(docker images --format " {{.Repository}}" "$1")
+    if [ "${rep_info}" -eq "$1" ]; then
+        docker image rm "$1"
+    fi
+}
+
 function clear_images() {
     # clear pre images of docker before rebuild
-    docker image rm localhost:5000/falconfs-cn:ubuntu24.04
-    docker image rm localhost:5000/falconfs-dn:ubuntu24.04
-    docker image rm localhost:5000/falconfs-store:ubuntu24.04
-    docker image rm localhost:5000/falconfs-regress:ubuntu24.04
+    clear_one_image "localhost:5000/falconfs-cn:ubuntu24.04"
+    clear_one_image "localhost:5000/falconfs-dn:ubuntu24.04"
+    clear_one_image "localhost:5000/falconfs-store:ubuntu24.04"
+    clear_one_image "localhost:5000/falconfs-regress:ubuntu24.04"
 }
 
 function run_regress() {
@@ -123,13 +137,13 @@ function run_regress() {
     docker-compose -f "${1}" up -d
 
     # wait for cluster ready
-    ready_flag=$(docker exec falcon-zk-1 zkCli.sh -server localhost:2181 ls /falcon | grep falcon_cn | grep -c ready)
+    ready_flag=$(docker exec falcon-zk-1 zkCli.sh -server localhost:2181 ls /falcon | awk '/ready/' | wc -l)
     waited_times=0
     sleep_interval=5
     while [ "${ready_flag}" -ne "1" ]; do
         echo "falcon cluster not ready, wait ${waited_times} second ... "
         sleep $sleep_interval
-        ready_flag=$(docker exec falcon-zk-1 zkCli.sh -server localhost:2181 ls /falcon | grep falcon_cn | grep -c ready)
+        ready_flag=$(docker exec falcon-zk-1 zkCli.sh -server localhost:2181 ls /falcon | awk '/ready/' | wc -l)
         ((waited_times += sleep_interval))
     done
     echo "falcon cluster ready now, cost ${waited_times} second."
@@ -140,13 +154,13 @@ function run_regress() {
     done
 
     # wait for filesystem of falcon store mounted
-    mounted_num=$(df -T | grep -c fuse.falcon_client)
+    mounted_num=$(df -T | awk '/fuse.falcon_client/' | wc -l)
     waited_times=0
     sleep_interval=5
     while [ "${mounted_num}" -ne "${FALCON_STORE_NUM}" ]; do
         echo "fuse.falcon_client file system not ready, wait ${waited_times} second ... "
         sleep $sleep_interval
-        mounted_num=$(df -T | grep -c fuse.falcon_client)
+        mounted_num=$(df -T | awk '/fuse.falcon_client/' | wc -l)
         ((waited_times += sleep_interval))
     done
     echo "fuse.falcon_client file system ready, cost ${waited_times} second."
@@ -178,11 +192,11 @@ compose_files=('docker-compose-single.yaml' 'docker-compose-dual.yaml' 'docker-c
 
 for compose_file in "${compose_files[@]}"; do
     echo "start regress test for ${compose_file}..."
-    # cleare pre run data
+    # clear pre run data
     clean_run_data
     # start regress from single replica
-    run_regress $compose_file
+    run_regress "${compose_file}"
     echo "end regress test for ${compose_file}..."
     # uninstall cluster
-    uninstall_cluster $compose_file
+    uninstall_cluster "${compose_file}"
 done
